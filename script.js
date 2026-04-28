9b24eef3753033a5d036e7bdd03ecb58ced6e8c4
Sidharth B. <sidharthtechworld2k@gmail.com>
2026-04-28 14:59:38 +0530

    Added search button

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function generateColor(){
    let color="#"+Math.floor(Math.random()*16777215).toString(16);
    document.querySelector(".color-box").style.backgroundColor=color;
-   document.getElementById("display").innerHTML=color;
-
+   document.getElementById("display").innerHTML=color;}
+function searchGoogle(){
+const searchQuery=document.getElementById("display").innerHTML;
+const encodedQuery=encodeURIComponent(searchQuery);
+const googleURL="https://www.google.com/search?q="+encodedQuery;
+window.location.href=googleURL;
 }
