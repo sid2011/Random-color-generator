9ac3dc476b3eeb60ce957f2c906622ccfd00a6fc
Sidharth B. <sidharthtechworld2k@gmail.com>
2026-04-28 13:58:59 +0530

    Dead-code removed

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function generateColor(){
    let color="#"+Math.floor(Math.random()*16777215).toString(16);
-   console.log( color);
    document.querySelector(".color-box").style.backgroundColor=color;
    document.getElementById("display").innerHTML=color;
 
